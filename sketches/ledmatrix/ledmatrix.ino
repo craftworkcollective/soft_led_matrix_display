01a781e484ce87bc374abdddeeb732b7a933d5c9
vkmanganiello <vkmanganiello@gmail.com>
2024-10-03 12:30:15 -0400

    created state machine

--- sketches/ledmatrix/ledmatrix.ino
+++ sketches/ledmatrix/ledmatrix.ino
@@ -1,10 +1,47 @@
+// ---  NEO PIXELS --- // 
+
+
+// ---  STATE MACHINE --- // 
+enum STATES {
+  LOADING,
+  ATTRACT,
+  SCROLLING_TEXT,
+  OFF,
+  NUM_STATES
+}
+
+STATES state = STATES.LOADING;
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // ---  STATE MACHINE --- // 
+  switch (state) {
+    case STATES.LOADING:
+      {
+        break;
+      }
+    case STATES.ATTRACT:
+      {
+        break;
+      }
+    case STATES.SCROLLING_TEXT:
+      {
+        break;
+      }
+    case STATES.OFF:
+      {
+        break;
+      }
+    default:
+      {
+        break;
+      }
+  }
 }
-//blah blah blah 
+//blah blah blah
