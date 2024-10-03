6883109f69b44fbaa21b26638e96fc048694cd26
vkmanganiello <vkmanganiello@gmail.com>
2024-10-03 12:43:09 -0400

    add neopixel library and color wipe for states

--- sketches/ledmatrix/ledmatrix.ino
+++ sketches/ledmatrix/ledmatrix.ino
@@ -1,41 +1,59 @@
-// ---  NEO PIXELS --- // 
+// ---  NEO PIXELS --- //
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
 
+#define LED_PIN 6
+#define LED_COUNT 30
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// ---  STATE MACHINE --- // 
+// ---  STATE MACHINE --- //
 enum STATES {
   LOADING,
   ATTRACT,
   SCROLLING_TEXT,
   OFF,
   NUM_STATES
-}
+};
 
-STATES state = STATES.LOADING;
+STATES state = LOADING;
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+
+  strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();             // Turn OFF all pixels ASAP
+  strip.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 void loop() {
 
-  // ---  STATE MACHINE --- // 
+  // ---  STATE MACHINE --- //
   switch (state) {
-    case STATES.LOADING:
+    case LOADING:
       {
+        colorWipe(strip.Color(255, 0, 0), 50);  // Red
         break;
       }
-    case STATES.ATTRACT:
+    case ATTRACT:
       {
+        colorWipe(strip.Color(0, 255, 0), 50);  // Green
         break;
       }
-    case STATES.SCROLLING_TEXT:
+    case SCROLLING_TEXT:
       {
+        colorWipe(strip.Color(0, 0, 255), 50);  // Blue
         break;
       }
-    case STATES.OFF:
+    case OFF:
       {
+        colorWipe(strip.Color(0, 0, 0), 0);
         break;
       }
     default:
@@ -44,4 +62,12 @@ void loop() {
       }
   }
 }
-//blah blah blah
+
+
+void colorWipe(uint32_t color, int wait) {
+  for (int i = 0; i < strip.numPixels(); i++) {  // For each pixel in strip...
+    strip.setPixelColor(i, color);               //  Set pixel's color (in RAM)
+    strip.show();                                //  Update strip to match
+    delay(wait);                                 //  Pause for a moment
+  }
+}
